1da49c38ba7a2631d0a95463fc2bbccda10319fc
Tomas Korcak <korczis@gmail.com>
2025-07-24 22:01:54 +0200

    feat: initial commit

---
+++ test/prismatic_web/controllers/page_controller_test.exs
@@ -0,0 +1,8 @@
+defmodule PrismaticWeb.PageControllerTest do
+  use PrismaticWeb.ConnCase
+
+  test "GET /", %{conn: conn} do
+    conn = get(conn, ~p"/")
+    assert html_response(conn, 200) =~ "Peace of mind from prototype to production"
+  end
+end
